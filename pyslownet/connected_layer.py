5232c35b53614f6a83e44354e9f8cdb03516eded
DeepBlue97 <945048933@qq.com>
2023-04-05 10:27:50 +0800

    conv

---
+++ pyslownet/connected_layer.py
@@ -0,0 +1,2 @@
+class ConnectedLayer:
+    pass
--- pyslownet/convolutional_layer.py
+++ pyslownet/convolutional_layer.py
@@ -1,7 +1,13 @@
-class Conv2d:
-    def __init__(self):
-        pass
 
+class ConvolutionalLayer:
+    def __init__(self, in_channel: int, out_channel: int, kernel_size: int,
+        padding: int, stride: int
+    ):
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.stride = stride
 
     def forward():
         pass
@@ -24,3 +30,6 @@ class Conv2d:
     def resize():
         pass
 
+if __name__ == "__main__":
+    # conv = ConvolutionalLayer(3, 16, (3, 3),  (3, 3),  (3, 3))
+    conv = ConvolutionalLayer(3, 16, 3, 3, 3)
